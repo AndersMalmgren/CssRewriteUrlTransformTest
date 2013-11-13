b8c2dcd5af968ec09957fb0b89c34f80d05f82d6
AndersMalmgren <anders.malmgren@agero.se>
2013-11-13 11:00:15 +0100

    Initial commit

---
+++ CssRewriteUrlTransformTest/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace CssRewriteUrlTransformTest.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            var cssBundle = new StyleBundle("~/bundles/css");
+            new[]
+                {
+                    "~/Content/test.css"
+                }
+                .ToList()
+                .ForEach(css => bundles.Add(cssBundle.Include(css, new CssRewriteUrlTransform())));
+        }
+    }
+}
